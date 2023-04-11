5352b56726e8812f9cd50f521962d8ef4ffc3ac6
DrewRosales <drrosales@gmail.com>
2023-04-10 21:17:13 -0400

    Removed duplicate files

--- init.lua
+++
@@ -1,8 +0,0 @@
-require("plugin-setup")
-require("core.options")
-require("core.keymaps")
-require("core.colorscheme")
-require("plugin.nvim-tree")
-require("plugin.lualine")
-require("plugin.nvim-cmp")
-require("plugin.lsp.mason")
